b5ec9c2922701ed437d7b3122d23baacd5879c06
Fernando C. Lohmann <107650854+Lohmann12@users.noreply.github.com>
2025-08-25 19:36:16 +0000

    Desafio Iniciante das Cartas de CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,54 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Desafio do jogo Super Trunfo Iniciante\n");
 
-    return 0;
+    char estado1[2];
+    char estado2[2];
+
+    char codEstado1[5];
+    char codEstado2[5];
+
+    char nome1[100];
+    char nome2[100];
+
+    int populacao1;
+    int populacao2;
+
+    float area1;
+    float area2;
+
+    float pib1;
+    float pib2;
+
+    printf("Escolha uma letra para seu 1° estado (A-H) \n");
+    scanf("%1s", estado1);
+    printf("Escolha um código para sua 1° carta (01-04) \n");
+    scanf("%4s", codEstado1);
+    printf("Qual o nome do seu 1° estado?\n");
+    scanf("%99s", nome1);
+    printf("Qual a população do seu 1° estado?\n");
+    scanf("%d", &populacao1);
+    printf("Qual o tamanho do seu 1° estado? (Em km²)\n");
+    scanf("%f", &area1);
+    printf("Qual o PIB do seu 1° estado?\n");
+    scanf("%f", &pib1);
+
+    printf("Escolha uma letra para seu 2° estado (A-H) \n");
+    scanf("%1s", estado2);
+    printf("Escolha um código para sua 2° carta (01-04) \n");
+    scanf("%4s", codEstado2);
+    printf("Qual o nome do seu 2° estado?\n");
+    scanf("%99s", nome2);
+    printf("Qual a população do seu 2° estado?\n");
+    scanf("%d", &populacao2);
+    printf("Qual o tamanho do seu 2° estado? (Em km²)\n");
+    scanf("%f", &area2);
+    printf("Qual o PIB do seu 2° estado?\n");
+    scanf("%f", &pib2);
+
+    printf("1° carta: %s%s - %s - População: %d - Área: %.2f km² - PIB: R$%.2f\n", estado1, codEstado1, nome1, populacao1, area1, pib1);
+    printf("2° carta: %s%s - %s - População: %d - Área: %.2f km² - PIB: R$%.2f\n", estado2, codEstado2, nome2, populacao2, area2, pib2);
+
+    retun = 0;
 }
