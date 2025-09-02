c8d0c91636fc5be922c8484e95f782ec6b4f6ea4
Fernando C. Lohmann <107650854+Lohmann12@users.noreply.github.com>
2025-09-02 18:34:16 +0000

    Nível intermediário completo, adição de densidade populacional e pib per capita.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+
 int main() {
-    printf("Desafio do jogo Super Trunfo Iniciante\n");
+    printf("Desafio do jogo Super Trunfo Intermediário\n");
 
     char pais1[2];
     char pais2[2];
@@ -21,6 +22,12 @@ int main() {
     float pib1;
     float pib2;
 
+    float densidadePopulacional1;
+    float densidadePopulacional2;
+
+    float pibPerCapita1;
+    float pibPerCapita2;
+
     printf("Escolha uma letra para seu 1° País (A-H) \n");
     scanf("%1s", pais1);
     printf("Escolha um código para sua 1° carta (01-04) \n");
@@ -33,6 +40,10 @@ int main() {
     scanf("%f", &area1);
     printf("Qual o PIB do seu 1° País?\n");
     scanf("%f", &pib1);
+        densidadePopulacional1 = populacao1 / area1;
+        pibPerCapita1 = pib1 / populacao1;
+    printf("A densidade populacional do %s é %.2f habitantes por km².\n", nome1, densidadePopulacional1);
+    printf("O PIB per capita do %s é de R$%.2f.\n", nome1, pibPerCapita1);
 
     printf("Escolha uma letra para seu 2° País (A-H) \n");
     scanf("%1s", pais2);
@@ -46,9 +57,13 @@ int main() {
     scanf("%f", &area2);
     printf("Qual o PIB do seu 2° País?\n");
     scanf("%f", &pib2);
+        densidadePopulacional2 = populacao2 / area2;
+        pibPerCapita2 = pib2 / populacao2;
+    printf("A densidade populacional do %s é %.2f habitantes por km².\n", nome2, densidadePopulacional2);
+    printf("O PIB per capita do %s é de R$%.2f.\n", nome2, pibPerCapita2);
 
-    printf("1° carta: %s%s - %s - População: %d Milhões - Área: %.2f km² - PIB: R$%.2f\n", pais1, codPais1, nome1, populacao1, area1, pib1);
-    printf("2° carta: %s%s - %s - População: %d Milhões - Área: %.2f km² - PIB: R$%.2f\n", pais2, codPais2, nome2, populacao2, area2, pib2);
+    printf("1° carta: %s%s - %s - População: %d Milhões - Área: %.2f km² - PIB: R$%.2f - Densidade Populacional: %.2f habitantes por km² - PIB per Capita: R$%.2f\n", pais1, codPais1, nome1, populacao1, area1, pib1, densidadePopulacional1, pibPerCapita1);
+    printf("2° carta: %s%s - %s - População: %d Milhões - Área: %.2f km² - PIB: R$%.2f - Densidade Populacional: %.2f habitantes por km² - PIB per Capita: R$%.2f\n", pais2, codPais2, nome2, populacao2, area2, pib2, densidadePopulacional2, pibPerCapita2);
 
     return 0;
 }
